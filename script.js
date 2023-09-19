6fb6ade543ae021abcb227b025fa94452ec4c80c
Ayush <ayushmanisharma@gmail.com>
2023-09-18 20:40:27 -0400

    created password generator

---
+++ script.js
@@ -0,0 +1,61 @@
+var myDocument = document;
+var Heading = myDocument.querySelector('h1');
+var Button = myDocument.querySelector('button');
+var Submit = myDocument.querySelector("#submit");
+var Generate = myDocument.querySelector("#generate");
+var Reset = myDocument.querySelector("#reset");
+
+function generatePassword() {
+
+    var password = "";
+    var lowercase = "abcdefghijklmnopqrstuvwxyz";
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numbers = "0123456789";
+    var symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    var passwordLength = parseInt(prompt("Enter the desired length of your password (between 8 and 128 characters):"));
+    var HasLowercase = prompt("Do you want to include lowercase letters in your password?");
+    var HasUppercase = prompt("Do you want to include uppercase letters in your password?");
+    var HasNumbers = prompt("Do you want to include numbers in your password?");
+    var HasSymbols = prompt("Do you want to include symbols in your password?");
+    
+
+    if (!(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+        alert("Please enter a number between 8 and 128.");
+        return "";
+    }
+
+    var charset = "";
+    if (HasLowercase == ("yes") || ("Y")) {
+        charset += lowercase;
+    }
+    if (HasUppercase == ("yes") || ("Y")) {
+        charset += uppercase;
+    }
+    if (HasNumbers == ("yes") || ("Y")) {
+        charset += numbers;
+    }
+    if (HasSymbols == ("yes") || ("Y")) {
+        charset += symbols;
+    }
+
+    if (charset === "") {
+        alert("You must select at least one character type for the password.");
+        return "";
+    }
+
+    for (var i = 0; i < passwordLength; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = myDocument.querySelector("#password");
+
+    passwordText.value = password;
+}
+
+generate.addEventListener("click", writePassword);
